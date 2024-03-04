c6d9270fbf337bbe46ae4fc198e620223dd8d067
webskyus <webskyus@gmail.com>
2024-03-04 14:11:57 +0400

    remix initial

---
+++ public/build/_shared/remix_hmr-RIMXEON4.js
@@ -0,0 +1,9 @@
+import {
+  createHotContext
+} from "/build/_shared/chunk-YGG66OMB.js";
+import "/build/_shared/chunk-K6PKGSTD.js";
+import "/build/_shared/chunk-PNG5AS42.js";
+export {
+  createHotContext
+};
+//# sourceMappingURL=/build/_shared/remix_hmr-RIMXEON4.js.map
