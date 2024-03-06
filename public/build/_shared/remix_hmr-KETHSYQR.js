8ba093d6564bfaac759553d9b3f44a53b1c6225a
webskyus <webskyus@gmail.com>
2024-03-06 16:00:30 +0400

    change gitignore

--- public/build/_shared/remix_hmr-KETHSYQR.js
+++
@@ -1,5 +0,0 @@
-import { createHotContext } from "/build/_shared/chunk-QWXUWYGZ.js";
-import "/build/_shared/chunk-K6PKGSTD.js";
-import "/build/_shared/chunk-PNG5AS42.js";
-export { createHotContext };
-//# sourceMappingURL=/build/_shared/remix_hmr-KETHSYQR.js.map
